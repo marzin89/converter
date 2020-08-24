5689d5dc7c907c91ed3b47133090a55eda55c6bc
Marco <dead_legend@hotmail.com>
2020-08-24 23:47:02 +0200

    Add function that updates the units in the drop-down menu according to the chosen tab/category.

--- converter.js
+++ converter.js
@@ -2,17 +2,25 @@ const length = document.getElementById('length');
 
 length.addEventListener('click', changeTabColor);
 
+length.addEventListener('click', updateUnits);
+
 const weight = document.getElementById('weight');
 
 weight.addEventListener('click', changeTabColor);
+
+weight.addEventListener('click', updateUnits);
 
 const volume = document.getElementById('volume');
 
 volume.addEventListener('click', changeTabColor);
 
+volume.addEventListener('click', updateUnits);
+
 const temperature = document.getElementById('temperature');
 
 temperature.addEventListener('click', changeTabColor);
+
+temperature.addEventListener('click', updateUnits);
 
 const optionOneMetric = document.getElementById('millimeters');
 
@@ -71,5 +79,81 @@ function changeTabColor() {
         volume.style.backgroundColor = 'lightsalmon';
 
         temperature.style.backgroundColor = 'lightsalmon';
+    }
+}
+
+function updateUnits() {
+
+    if (this.innerHTML == 'Weight') {
+
+        optionOneMetric.innerHTML = 'Milligrams';
+
+        optionTwoMetric.innerHTML = 'Grams';
+
+        optionThreeMetric.innerHTML = 'Kilograms';
+
+        optionFourMetric.innerHTML = 'Tons';
+
+        optionOneImperial.innerHTML = 'Ounces';
+
+        optionTwoImperial.innerHTML = 'Pounds';
+
+        optionThreeImperial.innerHTML = 'Short ton';
+
+        optionFourImperial.innerHTML = 'Long ton';
+
+    } else if (this.innerHTML == 'Volume') {
+
+        optionOneMetric.innerHTML = 'Milliliters';
+
+        optionTwoMetric.innerHTML = 'Centiliters';
+
+        optionThreeMetric.innerHTML = 'Deciliters';
+
+        optionFourMetric.innerHTML = 'Liters';
+
+        optionOneImperial.innerHTML = 'Cups';
+
+        optionTwoImperial.innerHTML = 'Pints';
+
+        optionThreeImperial.innerHTML = 'Quarts';
+
+        optionFourImperial.innerHTML = 'Gallons';
+
+    } else if (this.innerHTML == 'Temperature') {
+
+        optionOneMetric.innerHTML = 'Celsius';
+
+        optionTwoMetric.innerHTML = '';
+
+        optionThreeMetric.innerHTML = '';
+
+        optionFourMetric.innerHTML = '';
+
+        optionOneImperial.innerHTML = 'Farenheit';
+
+        optionTwoImperial.innerHTML = '';
+
+        optionThreeImperial.innerHTML = '';
+
+        optionFourImperial.innerHTML = '';
+
+    } else if (this.innerHTML = 'Length') {
+
+        optionOneMetric.innerHTML = 'Millimeters';
+
+        optionTwoMetric.innerHTML = 'Centimeters';
+
+        optionThreeMetric.innerHTML = 'Meters';
+
+        optionFourMetric.innerHTML = 'Kilometers';
+
+        optionOneImperial.innerHTML = 'Inches';
+
+        optionTwoImperial.innerHTML = 'Feet';
+
+        optionThreeImperial.innerHTML = 'Yards';
+
+        optionFourImperial.innerHTML = 'Miles';
     }
 }
