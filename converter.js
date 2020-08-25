3da0ab57baea88df0e0ae1b8d0d017023f0bcd10
Marco <dead_legend@hotmail.com>
2020-08-25 15:18:39 +0200

    Add function that switches the metric and imperial values when the button is clicked in order to allow for conversion in both directions.

--- converter.js
+++ converter.js
@@ -157,3 +157,190 @@ function updateUnits() {
         optionFourImperial.innerHTML = 'Miles';
     }
 }
+
+const switchBtn = document.getElementById('switch-imperial-metric');
+
+switchBtn.addEventListener('click', switchMetricImperial);
+
+const labelMetric = document.getElementById('label-metric');
+
+const labelImperial = document.getElementById('label-imperial');
+
+function switchMetricImperialLength() {
+
+    if (optionOneMetric.innerHTML == 'Millimeters') {
+
+        optionOneMetric.innerHTML = 'Inches';
+
+        optionTwoMetric.innerHTML = 'Feet';
+
+        optionThreeMetric.innerHTML = 'Yards';
+
+        optionFourMetric.innerHTML = 'Miles';
+
+        optionOneImperial.innerHTML = 'Millimeters';
+
+        optionTwoImperial.innerHTML = 'Centimeters';
+
+        optionThreeImperial.innerHTML = 'Meters';
+
+        optionFourImperial.innerHTML = 'Kilometers';
+
+        labelMetric.innerHTML = 'Imperial';
+
+        labelImperial.innerHTML = 'Metric';
+
+    } else if (optionOneMetric.innerHTML == 'Inches') {
+
+        optionOneMetric.innerHTML = 'Millimeters';
+
+        optionTwoMetric.innerHTML = 'Centimeters';
+
+        optionThreeMetric.innerHTML = 'Meters';
+
+        optionFourMetric.innerHTML = 'Kilometers';
+
+        optionOneImperial.innerHTML = 'Inches';
+
+        optionTwoImperial.innerHTML = 'Feet';
+
+        optionThreeImperial.innerHTML = 'Yards';
+
+        optionFourImperial.innerHTML = 'Miles';
+
+        labelMetric.innerHTML = 'Metric';
+
+        labelImperial.innerHTML = 'Imperial';
+    } 
+}
+
+function switchMetricImperialWeight() {
+
+    if (optionOneMetric.innerHTML == 'Milligrams') {
+
+        optionOneMetric.innerHTML = 'Ounces';
+
+        optionTwoMetric.innerHTML = 'Pounds';
+
+        optionThreeMetric.innerHTML = 'Short ton';
+
+        optionFourMetric.innerHTML = 'Long ton';
+
+        optionOneImperial.innerHTML = 'Milligrams';
+
+        optionTwoImperial.innerHTML = 'Grams';
+
+        optionThreeImperial.innerHTML = 'Kilograms';
+
+        optionFourImperial.innerHTML = 'Tons';
+
+        labelMetric.innerHTML = 'Imperial';
+
+        labelImperial.innerHTML = 'Metric';
+
+    } else if (optionOneMetric.innerHTML == 'Ounces') {
+
+        optionOneMetric.innerHTML = 'Milligrams';
+
+        optionTwoMetric.innerHTML = 'Grams';
+
+        optionThreeMetric.innerHTML = 'Kilograms';
+
+        optionFourMetric.innerHTML = 'Tons';
+
+        optionOneImperial.innerHTML = 'Ounces';
+
+        optionTwoImperial.innerHTML = 'Pounds';
+
+        optionThreeImperial.innerHTML = 'Short ton';
+
+        optionFourImperial.innerHTML = 'Long ton';
+
+        labelMetric.innerHTML = 'Metric';
+
+        labelImperial.innerHTML = 'Imperial';
+    }
+}
+
+function switchMetricImperialVolume() {
+
+    if (optionOneMetric.innerHTML == 'Milliliters') {
+
+        optionOneMetric.innerHTML = 'Cups';
+
+        optionTwoMetric.innerHTML = 'Pints';
+
+        optionThreeMetric.innerHTML = 'Quarts'
+
+        optionFourMetric.innerHTML = 'Gallons';
+
+        optionOneImperial.innerHTML = 'Milliliters';
+
+        optionTwoImperial.innerHTML = 'Centiliters'
+
+        optionThreeImperial.innerHTML = 'Deciliters';
+
+        optionFourImperial.innerHTML = 'Liters';
+
+        labelMetric.innerHTML = 'Imperial';
+
+        labelImperial.innerHTML = 'Metric';
+
+    } else if (optionOneMetric.innerHTML == 'Cups') {
+
+        optionOneMetric.innerHTML = 'Milliliters';
+
+        optionTwoMetric.innerHTML = 'Centiliters';
+
+        optionThreeMetric.innerHTML = 'Deciliters'
+
+        optionFourMetric.innerHTML = 'Liters';
+        
+        optionOneImperial.innerHTML = 'Cups';
+
+        optionTwoImperial.innerHTML = 'Pints'
+
+        optionThreeImperial.innerHTML = 'Quarts';
+
+        optionFourImperial.innerHTML = 'Gallons';
+
+        labelMetric.innerHTML = 'Metric';
+
+        labelImperial.innerHTML = 'Imperial';
+    }
+}
+
+function switchMetricImperialTemperature() {
+
+    if (optionOneMetric.innerHTML == 'Celsius') {
+
+        optionOneMetric.innerHTML = 'Farenheit';
+
+        optionOneImperial.innerHTML = 'Celsius';
+
+        labelMetric.innerHTML = 'Imperial';
+
+        labelImperial.innerHTML = 'Metric';
+
+    } else if (optionOneMetric.innerHTML == 'Farenheit') {
+
+        optionOneMetric.innerHTML = 'Celsius';
+
+        optionOneImperial.innerHTML = 'Farenheit';
+
+        labelMetric.innerHTML = 'Metric';
+
+        labelImperial.innerHTML = 'Imperial';
+    }
+}
+
+function switchMetricImperial() {
+    
+    switchMetricImperialLength();
+
+    switchMetricImperialWeight();
+
+    switchMetricImperialVolume();
+
+    switchMetricImperialTemperature();    
+}
